d91d2d2fd269d11f39f11861dcf74bc36b6b34e4
justinbaldauf <21261503+justinbaldauf@users.noreply.github.com>
2018-02-12 13:23:42 +0800

    search box

--- trackster.js
+++ trackster.js
@@ -1,8 +1,18 @@
+
+$(document).ready(function(){
+  $('header button').click(function(){
+    var searchInput = $('header input').val();
+    Trackster.searchTracksByTitle(searchInput);
+  });
+});
+
+API_KEY = '26a2316b2cbee1a267ac8659d7ee6bd1';
+
 var Trackster = {};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
@@ -13,5 +23,10 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax({
+    url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=Believe&api_key=API_KEY&format=json',
+    datatype:'jsonp',
+    success: function(){
+    }
+  });
 };
